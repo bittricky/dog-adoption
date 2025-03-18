6a9984b8ea14b8d72fad98d914f231857f65de8a
Mitul Patel <me@mitulpa.tel>
2025-03-18 13:37:43 -0400

    feat(types): structured types for app

---
+++ src/global.d.ts
@@ -0,0 +1,29 @@
+export interface Dog {
+  id: string;
+  img: string;
+  name: string;
+  age: number;
+  zip_code: string;
+  breed: string;
+}
+
+export interface Location {
+  zip_code: string;
+  latitude: number;
+  longitude: number;
+  city: string;
+  state: string;
+  county: string;
+}
+
+export interface SearchResponse {
+  resultIds: string[];
+  total: number;
+  next?: string;
+  prev?: string;
+}
+
+export interface LoginCredentials {
+  name: string;
+  email: string;
+}
